72ab40f296d0217268d0a0c795da191aa5f6914a
“niuniulogo” <463314200@qq.com>
2021-05-19 17:47:01 +0800

    底部导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <router-view />
+
+  <tab-bar></tab-bar>    
 </template>
 
 <script>
+import tabBar from "/@/components/tabBar.vue";
 export default {
   name: 'App',
+  components:{
+    tabBar
+  },
   setup() {
 
   }
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 	@import 'vant/lib/index.css'
 </style>
---
+++ src/components/tabBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <van-tabbar v-model="active" @change="onChange">
+    <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+    <van-tabbar-item icon="search" dot>标签</van-tabbar-item>
+    <van-tabbar-item icon="friends-o" badge="5">标签</van-tabbar-item>
+    <van-tabbar-item icon="setting-o" badge="20">标签</van-tabbar-item>
+  </van-tabbar>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ref } from 'vue';
+import { Toast } from 'vant';
+export default defineComponent({
+  setup() {
+    const active = ref(0);
+    const onChange = (index:number) => Toast(`标签 ${index}`);
+    return {
+      active,
+      onChange,
+    };
+  },
+});
+</script>
+<style lang="scss" scoped>
+</style>
+
